682cfb9bbcf4dc0dbe4f7bcd4c794126e8b4bd3b
Simon Liétar <simon@lietar.net>
2022-04-02 23:37:11 +0200

    Rewrite schema transformation

--- host/packages/runner/util/schema.py
+++ host/packages/runner/util/schema.py
@@ -1,4 +1,3 @@
-from venv import create
 from ..reader import LocatedValue
 
 
@@ -12,13 +11,19 @@ def create_error(obj, message):
 # Entry point for creating schemas for values provided by the user.
 def Schema(schema_like):
   if type(schema_like) == dict:
-    return SchemaDict(schema_like)
+    return Dict(schema_like)
   if callable(schema_like):
     return SchemaType(schema_like)
   if type(schema_like) in [type(None), int, str]:
     return Exact(schema_like)
 
   return schema_like
+
+
+class Invariant:
+  def transform(self, test):
+    self.validate(test)
+    return test
 
 
 
@@ -30,12 +35,9 @@ def Schema(schema_like):
 #   SchemaType(str)
 #   SchemaType(int)
 
-class SchemaType:
+class SchemaType(Invariant):
   def __init__(self, type):
     self._type = type
-
-  def transform(self, obj):
-    pass
 
   def validate(self, test):
     if isinstance(test, LocatedValue):
@@ -48,64 +50,43 @@ class SchemaType:
     return f"<{self._type.__name__}>"
 
 
-# SchemaDict - Dictionary with fixed keys
-# Example
-#   SchemaDict({ 'foo': str })
-
-class SchemaDict(SchemaType):
-  def __init__(self, raw_dict, *, allow_extra = True):
-    super().__init__(dict)
-
-    self._allow_extra = allow_extra
-    self._dict = { key: Schema(value) for key, value in raw_dict.items() }
-
-  def transform(self, obj):
-    # TODO: actually shouldn't raise if not a dict
-    super().validate(obj)
-
-    output = dict(obj)
-
-    for key, value in self._dict.items():
-      transformed = value.transform(obj.get(key))
-
-      if transformed is not None:
-        output[key] = transformed
-      else:
-        value.validate(obj.get(key))
-
-    return output
-
-  def validate(self, test):
-    super().validate(test)
-
-    for key, value in self._dict.items():
-      if not (key in test):
-        raise create_error(test, f"Missing key '{key}'")
-
-      value.validate(test[key])
-
-    if not self._allow_extra:
-      for key in test.keys():
-        if not (key in self._dict):
-          raise create_error(key, f"Extraneous key '{key}'")
-
-  def __repr__(self):
-    return "{ " + ", ".join([f"'{key}': {value}" for key, value in self._dict.items()]) + " }"
-
-
-# FreeDict
+# Dict
 # Examples
-#   FreeDict({ 34: 'foo', int: str })
-#   FreeDict({ Optional(34): 'foo', int: str })
+#   Dict({ 34: 'foo', int: str })
+#   Dict({ Optional(34): 'foo', int: str })
 
-class FreeDict(SchemaType):
-  def __init__(self, raw_dict, *, allow_extra = True):
+class Dict(SchemaType):
+  def __init__(self, raw_dict, *, allow_extra = False):
     super().__init__(dict)
 
     self._dict = { Schema(key): Schema(value) for key, value in raw_dict.items() }
 
     if allow_extra:
       self._dict[Any()] = Any()
+
+  def transform(self, test):
+    super().validate(test)
+    output = dict()
+
+    for test_key, test_value in test.items():
+      for key, value in self._dict.items():
+        try:
+          new_key = key.transform(test_key)
+        except:
+          continue
+
+        new_value = value.transform(test_value)
+        output[new_key] = new_value
+        break
+      else:
+        raise create_error(test, f"Extraneous key {repr(test_key)}")
+
+    for key, value in self._dict.items():
+      if isinstance(key, Exact) and (not isinstance(value, Optional)):
+        if not (key._value in test):
+          raise create_error(test, f"Missing key {key}")
+
+    return output
 
   def validate(self, test):
     super().validate(test)
@@ -120,10 +101,10 @@ class FreeDict(SchemaType):
         value.validate(test_value)
         break
       else:
-        raise create_error(test, f"Extraneous key {key}")
+        raise create_error(test, f"Extraneous key {repr(test_key)}")
 
     for key, value in self._dict.items():
-      if isinstance(key, Exact):
+      if isinstance(key, Exact) and (not isinstance(value, Optional)):
         if not (key._value in test):
           raise create_error(test, f"Missing key {key}")
 
@@ -131,18 +112,18 @@ class FreeDict(SchemaType):
     return "{ " + ", ".join([f"{key}: {value}" for key, value in self._dict.items()]) + " }"
 
 
-# Dict
+# SimpleDict
 # Example
-#   Dict(str, int)
+#   SimpleDict(str, int)
 
-class Dict(FreeDict):
-  def __init__(self, key, value, *, allow_extra=True):
+class SimpleDict(Dict):
+  def __init__(self, key, value, *, allow_extra = False):
     super().__init__({ key: value }, allow_extra=allow_extra)
 
 
 # Any
 
-class Any:
+class Any(Invariant):
   def validate(self, test):
     pass
 
@@ -152,7 +133,7 @@ class Any:
 
 # Never
 
-class Never:
+class Never(Invariant):
   def validate(self, test):
     raise create_error(test, "Invalid value")
 
@@ -165,7 +146,7 @@ class Never:
 #   Exact(34)
 #   Exact('foo')
 
-class Exact:
+class Exact(Invariant):
   def __init__(self, value):
     self._value = value
 
@@ -205,21 +186,22 @@ class Or:
     assert(atoms)
     self._atoms = [Schema(atom) for atom in atoms]
 
-  def transform(self, value):
+  def transform(self, test):
     for atom in self._atoms:
-      transformed = atom.transform(value)
+      try:
+        return atom.transform(test)
+      except:
+        pass
 
-      if transformed is not None:
-        return transformed
+    # Should raise
+    self.validate(test)
 
-    return None
-
-  def validate(self, value):
+  def validate(self, test):
     exception = None
 
     for atom in self._atoms:
       try:
-        atom.validate(value)
+        atom.validate(test)
         return
       except Exception as e:
         if not exception:
@@ -251,39 +233,33 @@ class Use:
     return f"<{self._validator.__name__}>"
 
 
-class Transform:
-  def __init__(self, transform, *, prevalidate = None):
-    self._prevalidate = Schema(prevalidate) if prevalidate else None
+# Transform
+
+class Transform(Never):
+  def __init__(self, transform, schema_like = Any()):
+    self._schema = Schema(schema_like)
     self._transform = transform
 
-  def transform(self, value):
-    if self._prevalidate:
-      self._prevalidate.validate(value)
+  def transform(self, test):
+    self._schema.validate(test)
+    return self._transform(test)
 
-    return self._transform(value)
 
-  def validate(self, value):
-    pass
-
+# Noneable
 
 class Noneable(Or):
   def __init__(self, schema_like):
     super().__init__(schema_like, None)
 
-  # def transform(self, value):
-  #   return self._arg.transform(value) if value is not None else None
 
-  # def validate(self, test):
-  #   if value is not None:
-  #     self._schema.validate(test)
-
-  # def __repr__(self):
-  #   return f"Optional({self._arg})"
-
+# Optional
 
 class Optional:
   def __init__(self, schema_like):
     self._schema = Schema(schema_like)
+
+  def transform(self, test):
+    return self._schema.transform(test)
 
   def validate(self, test):
     self._schema.validate(test)
@@ -299,20 +275,9 @@ class List(SchemaType):
     super().__init__(list)
     self._schema = Schema(schema_like)
 
-  def transform(self, value):
-    super().validate(value)
-
-    output = list(value)
-
-    for index, item in enumerate(value):
-      transformed = self._arg.transform(item)
-
-      if transformed is not None:
-        output[index] = transformed
-      else:
-        self._arg.validate(item)
-
-    return output
+  def transform(self, test):
+    super().validate(test)
+    return [self._schema.transform(item) for item in test]
 
   def validate(self, test):
     super().validate(test)
@@ -331,29 +296,19 @@ class Tuple(SchemaType):
     super().__init__(list)
     self._tuple = [Schema(item) for item in items]
 
-  def transform(self, value):
-    super().validate(value)
+  def transform(self, test):
+    super().validate(test)
 
-    if len(value) != len(self._arg):
-      raise Exception(f"Invalid number of items, found {len(value)}, expected {len(self._arg)}")
+    if len(test) != len(self._tuple):
+      raise create_error(test, f"Invalid number of items, found {len(test)}, expected {len(self._tuple)}")
 
-    output = list(value)
-
-    for index, (provided, expected) in enumerate(zip(value, self._arg)):
-      transformed = expected.transform(provided)
-
-      if transformed is not None:
-        output[index] = transformed
-      else:
-        expected.validate(provided)
-
-    return output
+    return [item.transform(test_item) for test_item, item in zip(test, self._tuple)]
 
   def validate(self, test):
     super().validate(test)
 
     if len(test) != len(self._tuple):
-      raise Exception(f"Invalid number of items, found {len(test)}, expected {len(self._tuple)}")
+      raise create_error(test, f"Invalid number of items, found {len(test)}, expected {len(self._tuple)}")
 
     for test_item, item in zip(test, self._tuple):
       item.validate(test_item)
@@ -361,82 +316,44 @@ class Tuple(SchemaType):
   def __repr__(self):
     return "[" + ", ".join([str(item) for item in self._tuple]) + "]"
 
-# class Dict(SchemaType):
-#   def __init__(self, key, value):
-#     super().__init__(dict)
-#     self._key = Schema(key)
-#     self._value = Schema(value)
 
-#   def validate(self, obj):
-#     super().validate(obj)
-
-#     for key, value in obj.items():
-#       self._key.validate(key)
-#       self._value.validate(value)
-
-#   def __repr__(self):
-#     return f"{{ [{self._key}]: {self._value} }}"
-
-
-# Sequence
-
-class Sequence:
-  pass
-
+# ParseType
 
 class ParseType(SchemaType):
-  def transform(self, value):
+  def transform(self, test):
     if self._type == bool:
-      if (value == "true") or (value == "false"):
-        return value == "true"
+      if (test == "true") or (test == "false"):
+        return test == "true"
     elif self._type == int:
       try:
-        return int(value)
+        return int(test)
       except:
         pass
 
-    return None
+    super().validate(test)
+    return test
 
+
+## Tests ####
 
 if __name__ == "__main__":
   from .. import reader
 
   s = Schema({
     # 'foo': Array([ParseType(Or(bool, int)), Or(ParseType(bool), ParseType(int))])
-    'foo': int,
-    'bar': {
-      'x': And(int, Or(34, 35)),
-      'y': Use(lambda x: x > 5)
-    },
-    'baz': FreeDict({
-      Optional('a'): 'b',
-      's': List(15),
-      Any(): Noneable(Tuple([3, 4, int]))
-    })
+    # ParseType(int): Or(ParseType(int), ParseType(bool))
+    # 'foo': Or(bool, Transform(lambda x: str(x) * 2, int))
+    # 'bar': Tuple([ParseType(int), str])
+    Optional('foo'): int,
+    'bar': Optional(int)
   })
 
-  t = reader.parse("""
-x: 5
-y: 4
-  """)
-
   t = {
-    'foo': 4,
-    'bar': {
-      'x': 34,
-      'y': 16
-    },
-    'baz': {
-      # 'a': 'b',
-      's': [15, 15],
-      'p': [3, 4, 9]
-    }
+
   }
 
-  print(s)
-
   try:
-    s.validate(t)
+    print(s.transform(t))
   except reader.LocatedError as e:
     e.display()
 

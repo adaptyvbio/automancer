cc5f6b9c5edc2d909eacb31bca7a9e6296d64002
Simon Liétar <simon@lietar.net>
2022-06-01 23:49:32 +0200

    Add a diagram display to the chip view

---
+++ client/src/components/diagram.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+
+import { ControlNamespace } from '../backends/common';
+import * as util from '../util';
+
+
+export interface DiagramProps {
+  sheet: ControlNamespace.Sheet;
+  signal: bigint;
+  targetValveIndex: number | null;
+}
+
+export class Diagram extends React.Component<DiagramProps> {
+  ref = React.createRef<HTMLDivElement>();
+
+  componentDidMount() {
+    this.update();
+  }
+
+  componentDidUpdate(prevProps: Readonly<DiagramProps>, prevState: Readonly<{}>) {
+    this.update(prevProps.signal, prevProps.targetValveIndex);
+  }
+
+  update(prevSignal: bigint = 0n, prevTargetValveIndex: number | null = null) {
+    let createTargetMask = (valveIndex: number | null) =>
+      valveIndex !== null
+        ? (1n << BigInt(valveIndex))
+        : 0n;
+
+    let diff = (this.props.signal ^ prevSignal)
+      | (createTargetMask(this.props.targetValveIndex) ^ createTargetMask(prevTargetValveIndex));
+
+    for (let [valveIndex, valve] of this.props.sheet.valves.entries()) {
+      let mask = BigInt(1 << valveIndex);
+      let changed = (diff & mask) > 0;
+
+      if (changed && valve.diagramRef) {
+        let value = (this.props.signal & mask) > 0;
+        let element = this.ref.current!.querySelector(`[data-layer-index="${valve.diagramRef[0]}"][data-group-index="${valve.diagramRef[1]}"]`)!;
+
+        element.classList.toggle('_active', value);
+        element.classList.toggle('_target', valveIndex === this.props.targetValveIndex);
+      }
+    }
+  }
+
+  render() {
+    // let filter = `
+    // <filter id="demo4">
+    //     <!--Blur effect-->
+    //     <feGaussianBlur stdDeviation="3" result="blur4" />
+    //     <!--Lighting effect-->
+    //     <feSpecularLighting result="spec4" in="blur4" specularExponent="35" lighting-color="#cccccc">
+    //         <!--Light source effect-->
+    //         <fePointLight x="75" y="100" z="200">
+    //           <!--Lighting Animation-->
+    //           <animate attributeName="x" values="75;320;75" dur="10s" repeatCount="indefinite" />
+    //         </fePointLight>
+    //     </feSpecularLighting>
+    //     <!--Composition of inputs-->
+    //     <feComposite in="SourceGraphic" in2="spec4" operator="arithmetic" k1="0" k2="1" k3="1" k4="0" />
+    //   </filter>
+    //   <filter id="demo5">
+    //     <feGaussianBlur in="SourceGraphic" stdDeviation="2" result="blur5" />
+    //     <!--Composition of inputs-->
+    //     <feComposite in="SourceGraphic" in2="blur5" operator="arithmetic" k1="0" k2="3" k3="3" k4="0" />
+    //   </filter>
+    //   <filter id="demo6">
+    //   <feColorMatrix type="matrix" values=
+    //             "1 0 0 0   0
+    //              0 0 0 0   0
+    //              0 0 0 0   0
+    //              0 0 0 0.7 0"/>
+    // <feGaussianBlur stdDeviation="8.5" result="coloredBlur"/>
+    // <feMerge>
+    //     <feMergeNode in="coloredBlur"/>
+    //     <feMergeNode in="SourceGraphic"/>
+    // </feMerge>
+    //   </filter>`;
+
+    return (
+      <div
+        className={util.formatClass('diagram', { '_target': (this.props.targetValveIndex !== null) })}
+        dangerouslySetInnerHTML={{ __html: this.props.sheet.diagram! }}
+        // dangerouslySetInnerHTML={{ __html: this.props.sheet.diagram!.slice(0, -6) + filter + '</svg>' }}
+        ref={this.ref} />
+    );
+  }
+}
--- client/src/views/chip.tsx
+++ client/src/views/chip.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import type { Host, Route } from '../application';
 import { Chip, ChipId, ChipModel, ControlNamespace } from '../backends/common';
+import { Diagram } from '../components/diagram';
 import { Pool } from '../util';
 import * as util from '../util';
 
@@ -12,8 +13,20 @@ export interface ViewChipProps {
   setRoute(route: Route): void;
 }
 
-export class ViewChip extends React.Component<ViewChipProps> {
+export interface ViewChipState {
+  targetValveIndex: number | null;
+}
+
+export class ViewChip extends React.Component<ViewChipProps, ViewChipState> {
   pool = new Pool();
+
+  constructor(props: ViewChipProps) {
+    super(props);
+
+    this.state = {
+      targetValveIndex: null
+    };
+  }
 
   get chip(): Chip {
     return this.props.host.state.chips[this.props.chipId];
@@ -29,134 +42,93 @@ export class ViewChip extends React.Component<ViewChipProps> {
     let runner = this.chip.runners.control;
     let signal = BigInt(runner.signal);
 
-    console.log(matrix, sheet, runner);
+    // console.log(matrix, sheet, runner);
 
     return (
       <main>
         <h1>{this.chip.name}</h1>
 
-        <div className="header2">
-          <h2>Manual control</h2>
-        </div>
+        <div className="vchip-root">
+          {sheet.diagram && (
+            <>
+              <div className="header2">
+                <h2>Diagram</h2>
+              </div>
+              <div className="vchip-diagram">
+                <Diagram
+                  sheet={sheet}
+                  signal={signal}
+                  targetValveIndex={this.state.targetValveIndex} />
+              </div>
+            </>
+          )}
 
-        <div className="mcontrol-root">
-          {sheet.groups.map((group, groupIndex) => (
-            <React.Fragment key={groupIndex}>
-              <h3 className="mcontrol-grouptitle">{group.name}</h3>
-              <div className="mcontrol-group">
-                {Array.from(sheet.valves.entries())
-                  .filter(([_valveIndex, valve]) => valve.group === groupIndex)
-                  .map(([valveIndex, valve]) => {
-                    let active = ((1n << BigInt(valveIndex)) & signal) > 0;
-                    let modelValveMask = 1n << BigInt(valveIndex);
-                    // let active = Math.random() > 0.5;
-                    let status = runner.valves[valveIndex];
+          <div className="header2">
+            <h2>Manual control</h2>
+          </div>
 
-                    return (
-                      <div className={util.formatClass('mcontrol-entry', { '_on': active })} key={valveIndex}>
-                        <div className="mcontrol-icon">
-                          <span className="material-symbols-rounded">air</span>
-                        </div>
-                        <div className="mcontrol-label">{valve.names[0]}</div>
-                        <div className="mcontrol-sublabel">inlet/1</div>
-                        <div className="mcontrol-statuses">
-                          {(status.error !== null) && (
-                            <div className="mcontrol-status mcontrol-status--warning">
-                              <div className="mcontrol-status-icon">
-                                <span className="material-symbols-rounded">error</span>
-                              </div>
-                              <div className="mcontrol-status-label">{ControlNamespace.RunnerValveError[status.error]}</div>
-                            </div>
-                          )}
-                        </div>
-                        <div className="mcontrol-switches">
-                          <button type="button" className="mcontrol-switch" onClick={() => {
-                            this.props.host.backend.command(this.chip.id, {
-                              control: {
-                                type: 'signal',
-                                signal: String((signal! & ~modelValveMask) | (modelValveMask * BigInt(active ? 0 : 1)))
-                              }
-                            });
-                          }}>
-                            <div className="mcontrol-switch-icon">
+          <div className="mcontrol-root">
+            {sheet.groups.map((group, groupIndex) => (
+              <React.Fragment key={groupIndex}>
+                <div className="mcontrol-group">
+                  <h3 className="mcontrol-group-title">{group.name}</h3>
+                  <div className="mcontrol-group-entries">
+                    {Array.from(sheet.valves.entries())
+                      .filter(([_valveIndex, valve]) => valve.group === groupIndex)
+                      .map(([valveIndex, valve]) => {
+                        let active = ((1n << BigInt(valveIndex)) & signal) > 0;
+                        let modelValveMask = 1n << BigInt(valveIndex);
+                        let status = runner.valves[valveIndex];
+
+                        return (
+                          <div
+                            className={util.formatClass('mcontrol-entry', { '_on': active })}
+                            key={valveIndex}
+                            onMouseEnter={() => {
+                              this.setState({ targetValveIndex: valveIndex });
+                            }}
+                            onMouseLeave={() => {
+                              this.setState({ targetValveIndex: null });
+                            }}>
+                            <div className="mcontrol-icon">
                               <span className="material-symbols-rounded">air</span>
                             </div>
-                            <div className="mcontrol-switch-label">{active ? 'On' : 'Off'}</div>
-                          </button>
-                        </div>
-                      </div>
-                    );
-                  })}
-              </div>
-            </React.Fragment>
-          ))}
-        </div>
-
-        {/* <div className="mcontrol-root">
-          <div className="mcontrol-group">
-            <div className="mcontrol-entry">
-              <div className="mcontrol-icon">
-                <span className="material-symbols-rounded">air</span>
-              </div>
-              <div className="mcontrol-label">Inlet 1</div>
-              <div className="mcontrol-sublabel">inlet/1</div>
-              <div className="mcontrol-statuses">
-                <div className="mcontrol-status mcontrol-status--warning">
-                  <div className="mcontrol-status-icon">
-                    <span className="material-symbols-rounded">error</span>
+                            <div className="mcontrol-label">{valve.names[0]}</div>
+                            <div className="mcontrol-sublabel">inlet/1</div>
+                            <div className="mcontrol-statuses">
+                              {(status.error !== null) && (
+                                <div className="mcontrol-status mcontrol-status--warning">
+                                  <div className="mcontrol-status-icon">
+                                    <span className="material-symbols-rounded">error</span>
+                                  </div>
+                                  <div className="mcontrol-status-label">{ControlNamespace.RunnerValveError[status.error]}</div>
+                                </div>
+                              )}
+                            </div>
+                            <div className="mcontrol-switches">
+                              <button type="button" className="mcontrol-switch" onClick={() => {
+                                this.props.host.backend.command(this.chip.id, {
+                                  control: {
+                                    type: 'signal',
+                                    signal: String((signal! & ~modelValveMask) | (modelValveMask * BigInt(active ? 0 : 1)))
+                                  }
+                                });
+                              }}>
+                                <div className="mcontrol-switch-icon">
+                                  <span className="material-symbols-rounded">air</span>
+                                </div>
+                                <div className="mcontrol-switch-label">{active ? 'On' : 'Off'}</div>
+                              </button>
+                            </div>
+                          </div>
+                        );
+                      })}
                   </div>
-                  <div className="mcontrol-status-label">Unbound</div>
                 </div>
-                <div className="mcontrol-status">
-                  <div className="mcontrol-status-icon">
-                    <span className="material-symbols-rounded">motion_photos_auto</span>
-                  </div>
-                  <div className="mcontrol-status-label">Automatic</div>
-                </div>
-              </div>
-              <div className="mcontrol-switches">
-                <button type="button" className="mcontrol-switch">
-                  <div className="mcontrol-switch-icon">
-                    <span className="material-symbols-rounded">air</span>
-                  </div>
-                  <div className="mcontrol-switch-label">Off</div>
-                </button>
-              </div>
-            </div>
-            <div className="mcontrol-entry">
-              <div className="mcontrol-icon">
-                <span className="material-symbols-rounded">air</span>
-              </div>
-              <div className="mcontrol-label">Inlet 1</div>
-              <div className="mcontrol-sublabel">inlet/1</div>
-              <div className="mcontrol-statuses" />
-              <div className="mcontrol-switches">
-                <button type="button" className="mcontrol-switch">
-                  <div className="mcontrol-switch-label">Set auto</div>
-                </button>
-                <button type="button" className="mcontrol-switch">
-                  <div className="mcontrol-switch-icon">
-                    <span className="material-symbols-rounded">download</span>
-                  </div>
-                  <div className="mcontrol-switch-label">Off</div>
-                </button>
-              </div>
-            </div>
-            <div className="mcontrol-entry _on">
-              <div className="mcontrol-icon">
-                <span className="material-symbols-rounded">air</span>
-              </div>
-              <div className="mcontrol-label">Inlet 1</div>
-              <div className="mcontrol-sublabel">inlet/1</div>
-              <button type="button" className="mcontrol-switch">
-                <div className="mcontrol-switch-icon">
-                  <span className="material-symbols-rounded">download</span>
-                </div>
-                <div className="mcontrol-switch-label">On</div>
-              </button>
-            </div>
+              </React.Fragment>
+            ))}
           </div>
-        </div> */}
+        </div>
       </main>
     );
   }

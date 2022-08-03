9b3f2a766ca1f4932ce604bcfaed3132c4dc5670
Simon Liétar <simon@lietar.net>
2022-08-03 18:27:09 +0200

    Improve unit management

--- host/pr1/unit.py
+++ host/pr1/unit.py
@@ -1,43 +1,72 @@
 import importlib
 import importlib.metadata
+import importlib.util
 import inspect
-import logging
-from collections import namedtuple
-from pprint import pprint
+import sys
 
 from . import logger
 
 
-UnitInfo = namedtuple("UnitInfo", ["development", "module", "name", "package_name", "package_version", "path", "unit"])
+class UnitInfo:
+  def __init__(self, *, module, path, source_name, unit):
+    self.development = False
+    self.enabled = True
+    self.module = module
+    self.path = path
+    self.source_name = source_name
+    self.unit = unit
+
+    try:
+      metadata = importlib.metadata.metadata(module)
+    except importlib.metadata.PackageNotFoundError:
+      self.package_name = None
+      self.package_version = None
+    else:
+      self.package_name = metadata.get('name')
+      self.package_version = metadata.get('version')
+
+  @property
+  def name(self):
+    return self.unit.name
 
 class UnitManager:
   def __init__(self, conf):
     self.load(conf)
 
   def load(self, conf):
+    units_info = list()
+
+    for name, unit_conf in conf.items():
+      if 'module' in unit_conf:
+        module = unit_conf['module'].value
+
+        if 'path' in unit_conf:
+          spec = importlib.util.spec_from_file_location(module, unit_conf['path'].value)
+          unit = importlib.util.module_from_spec(spec)
+          sys.modules[module] = unit
+          spec.loader.exec_module(unit)
+        else:
+          unit = importlib.import_module(module)
+
+        units_info.append(UnitInfo(
+          module=module,
+          path=unit.__file__,
+          source_name=name,
+          unit=unit
+        ))
+
     # TODO: use entry_points() filtering when moving to Python 3.10
     # See https://docs.python.org/3/library/importlib.metadata.html
 
     relevant_entry_points = list(importlib.metadata.entry_points().get("pr1.units"))
-    units_info = list()
 
     for entry_point in relevant_entry_points:
       unit = entry_point.load()
 
-      if unit.name != entry_point.name:
-        logger.warn(f"Invalid name '{unit.name}' for entry point '{entry_point.name}'")
-        logger.warn("This unit will be ignored.")
-        continue
-
-      metadata = importlib.metadata.metadata(entry_point.module)
-
       units_info.append(UnitInfo(
-        development=False,
         module=entry_point.module,
-        name=unit.name,
-        package_name=metadata.get('name'),
-        package_version=metadata.get('version'),
         path=inspect.getfile(unit),
+        source_name=entry_point.name,
         unit=unit
       ))
 
@@ -48,10 +77,29 @@ class UnitManager:
       name = unit_info.name
 
       if name in self.units:
-        logger.warn(f"Duplicate unit name '{name}'")
+        logger.warn(f"Duplicate unit with name '{name}'")
         logger.warn("This unit will be ignored.")
+        continue
 
-      self.units[name] = unit_info.unit
+      if name != unit_info.source_name:
+        logger.warn(f"Invalid name '{name}' for source with name '{unit_info.source_name}'")
+        logger.warn("This unit will be ignored.")
+        continue
+
+      if name in conf:
+        unit_conf = conf[name]
+        unit_info.development = unit_conf.get('development', False)
+        unit_info.enabled = unit_conf.get('enabled', True)
+
       self.units_info[name] = unit_info
 
-      logger.debug(f"Loaded unit '{name}' from module '{unit_info.module}' and package '{unit_info.package_name}' with version '{unit_info.package_version}'")
+      logger.debug(f"Registered unit '{name}' from module '{unit_info.module}'" + (f" and package '{unit_info.package_name}' with version '{unit_info.package_version}'" if unit_info.package_name else str()))
+
+    for unit_info in self.units_info.values():
+      if unit_info.enabled:
+        self.units[unit_info.name] = unit_info.unit
+
+        if unit_info.development:
+          logger.info(f"Loaded unit '{unit_info.name}' in development mode")
+        else:
+          logger.debug(f"Loaded unit '{unit_info.name}'")

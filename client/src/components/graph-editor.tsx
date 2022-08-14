7b2b61915a2fa6cccff3e5c8f88e356dd2e60cd2
Simon Liétar <simon@lietar.net>
2022-08-14 14:27:52 +0200

    Improve the graph editor node structure

--- client/src/components/graph-editor.tsx
+++ client/src/components/graph-editor.tsx
@@ -79,19 +79,37 @@ export class GraphEditor extends React.Component<GraphEditorProps, GraphEditorSt
 
   render() {
     if (!this.state.size) {
-      return <div ref={this.refContainer} />;
+      return <div className="geditor-root" ref={this.refContainer} />;
     }
 
-    let cellSize = 30;
+    let styles = this.refContainer.current!.computedStyleMap();
+    // console.log(Object.fromEntries(Array.from(styles)));
+    let cellSize = CSSNumericValue.parse(styles.get('--cell-size')!).value;
+    let nodeHeaderHeight = CSSNumericValue.parse(styles.get('--node-header-height')!).value;
+    let nodePadding = CSSNumericValue.parse(styles.get('--node-padding')!).value;
+
     let cellCountX = Math.floor(this.state.size.width / cellSize);
     let cellCountY = Math.floor(this.state.size.height / cellSize);
 
-    let nodeOverflow = 15;
-    let nodeWidth = 8;
+    let nodeWidth = Math.round((220 + nodePadding * 2) / cellSize);
+    let nodeHeight = Math.ceil((nodeHeaderHeight + 80 + nodePadding * 2) / cellSize);
+
+    let link = {
+      start: { x: 11, y: 6 },
+      end: { x: 18, y: 3 }
+    };
+
+    let settings = {
+      cellSize,
+      nodeHeaderHeight,
+      nodePadding,
+      nodeWidth,
+      nodeHeight
+    };
 
     return (
-      <div ref={this.refContainer}>
-        <svg viewBox={`0 0 ${this.state.size.width} ${this.state.size.height}`} style={{ backgroundColor: '#f1f1f1' }}>
+      <div className="geditor-root" ref={this.refContainer}>
+        <svg viewBox={`0 0 ${this.state.size.width} ${this.state.size.height}`} className="geditor-svg">
           <g>
             {new Array(cellCountX * cellCountY).fill(0).map((_, index) => {
               let x = index % cellCountX;
@@ -100,13 +118,15 @@ export class GraphEditor extends React.Component<GraphEditorProps, GraphEditorSt
             })}
           </g>
 
+          <Link link={link} settings={settings} />
+
           {this.state.nodes.map((node) => (
             <React.Fragment key={node.id}>
               <foreignObject
-                x={cellSize * node.position.x - nodeOverflow}
-                y={cellSize * node.position.y - nodeOverflow}
-                width={cellSize * nodeWidth + nodeOverflow * 2}
-                height={cellSize * 5 + nodeOverflow * 2}
+                x={cellSize * node.position.x}
+                y={cellSize * node.position.y}
+                width={cellSize * nodeWidth}
+                height={cellSize * nodeHeight}
                 className="geditor-nodeobject">
                 <div className="geditor-node">
                   <div className="geditor-header">
@@ -122,9 +142,17 @@ export class GraphEditor extends React.Component<GraphEditorProps, GraphEditorSt
                   </div>
                 </div>
               </foreignObject>
+
               <circle
-                cx={cellSize * (node.position.x + nodeWidth) + nodeOverflow - 1}
-                cy={cellSize * node.position.y - nodeOverflow + 1 + 20}
+                cx={cellSize * node.position.x + nodePadding}
+                cy={cellSize * node.position.y + nodePadding + nodeHeaderHeight * 0.5}
+                r="5"
+                fill="#fff"
+                stroke="#000"
+                strokeWidth="2" />
+              <circle
+                cx={cellSize * (node.position.x + nodeWidth) - nodePadding}
+                cy={cellSize * node.position.y + nodePadding + nodeHeaderHeight * 0.5}
                 r="5"
                 fill="#fff"
                 stroke="#000"
@@ -154,11 +182,62 @@ export class GraphEditor extends React.Component<GraphEditorProps, GraphEditorSt
             </div>
           </foreignObject> */}
 
-          <path d={`M${cellSize * 10} ${cellSize * 12}L${cellSize * 12} ${cellSize * 12}Q${cellSize * 13} ${cellSize * 12} ${cellSize * 13} ${cellSize * 11}L${cellSize * 13} ${cellSize * 8}`} fill="none" stroke="#000" strokeLinecap="round" strokeWidth="2" />
+          {/* <path d={`M${cellSize * 10} ${cellSize * 12}L${cellSize * 12} ${cellSize * 12}Q${cellSize * 13} ${cellSize * 12} ${cellSize * 13} ${cellSize * 11}L${cellSize * 13} ${cellSize * 8}`} fill="none" stroke="#000" strokeLinecap="round" strokeWidth="2" />
           <path d={`M${cellSize * 10} ${cellSize * 12}L${cellSize * 15} ${cellSize * 12}`} stroke="#f1f1f1" strokeLinecap="round" strokeWidth="5" />
-          <path d={`M${cellSize * 10} ${cellSize * 12}L${cellSize * 15} ${cellSize * 12}`} stroke="#000" strokeLinecap="round" strokeWidth="2" />
+          <path d={`M${cellSize * 10} ${cellSize * 12}L${cellSize * 15} ${cellSize * 12}`} stroke="#000" strokeLinecap="round" strokeWidth="2" /> */}
         </svg>
       </div>
     );
   }
+}
+
+
+interface LinkIntf {
+  start: { x: number; y: number; };
+  end: { x: number; y: number; };
+}
+
+interface Settings {
+  cellSize: number;
+  nodeHeaderHeight: number;
+  nodePadding: number;
+  nodeWidth: number;
+  nodeHeight: number;
+}
+
+function Link(props: {
+  link: LinkIntf;
+  settings: Settings;
+}) {
+  let { link, settings } = props;
+
+  let startX = settings.cellSize * link.start.x - settings.nodePadding;
+  let startY = settings.cellSize * link.start.y;
+
+  let endX = settings.cellSize * link.end.x + settings.nodePadding;;
+  let endY = settings.cellSize * link.end.y;
+
+  let d = `M${startX} ${startY}`;
+
+  if (link.end.y !== link.start.y) {
+    let dir = (link.start.y < link.end.y) ? 1 : -1;
+
+    let midCellX = Math.round((link.start.x + link.end.x) * 0.5);
+    let midX = settings.cellSize * midCellX;
+
+    let midStartX = settings.cellSize * (midCellX - 1);
+    let midEndX = settings.cellSize * (midCellX + 1);
+
+    let curveStartY = settings.cellSize * (link.start.y + 1 * dir);
+    let curveEndY = settings.cellSize * (link.end.y - 1 * dir);
+
+    // d += `L${midX} ${startY}L${midX} ${endY}`;
+    d += `L${midStartX} ${startY}Q${midX} ${startY} ${midX} ${curveStartY}L${midX} ${curveEndY}Q${midX} ${endY} ${midEndX} ${endY}`;
+    // d += `L${midStartX} ${startY}q${settings.cellSize} 0 ${settings.cellSize} ${settings.cellSize}L${midX} ${curveEndY}q0 ${settings.cellSize} ${settings.cellSize} ${settings.cellSize}`;
+    // d += `L${midStartX} ${startY}q${cellSize} 0 ${cellSize} ${cellSize}L${midX} ${curveEndY}q0 ${cellSize} ${cellSize} ${cellSize}`;
+  }
+
+  d += `L${endX} ${endY}`;
+
+  return <path d={d} fill="none" stroke="#000" strokeLinecap="round" strokeWidth="2" />
 }

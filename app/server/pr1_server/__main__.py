08abca2e5f32994c30cf42bddb5892a9ad7f2da7
Simon Liétar <simon@lietar.net>
2022-08-15 17:53:03 +0200

    Add color to the server logs

--- app/server/pr1_server/__main__.py
+++ app/server/pr1_server/__main__.py
@@ -1,9 +1,25 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)-8s :: %(name)-18s :: %(message)s")
 
-for handler in logging.root.handlers:
-  handler.addFilter(logging.Filter("pr1"))
+class ColoredFormatter(logging.Formatter):
+  def format(self, record):
+    reset = "\x1b[0m"
+    color = {
+      logging.CRITICAL: "\x1b[31;1m",
+      logging.INFO: "\x1b[34;20m",
+      logging.ERROR: "\x1b[31;20m",
+      logging.WARNING: "\x1b[33;20m"
+    }.get(record.levelno, str())
+
+    formatter = logging.Formatter(f"{color}%(levelname)-8s{reset} :: %(name)-18s :: %(message)s")
+    return formatter.format(record)
+
+ch = logging.StreamHandler()
+ch.setFormatter(ColoredFormatter())
+
+logging.getLogger().addHandler(ch)
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pr1").setLevel(logging.DEBUG)
 
 
 from . import main

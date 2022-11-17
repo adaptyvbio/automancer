8a7818b92313de2351710547968b8fdd9c36ef14
Simon Liétar <simon@lietar.net>
2022-11-17 17:28:46 +0100

    Improve the sequence program

--- host/pr1/fiber/parsers/sequence.py
+++ host/pr1/fiber/parsers/sequence.py
@@ -1,13 +1,13 @@
+from dataclasses import dataclass
+from enum import IntEnum
 from types import EllipsisType
 from typing import Any, Optional
-
-from ..process import ProgramExecEvent
-
-from ...reader import LocationArea
 
 from ..eval import EvalEnvs, EvalStack
 from .. import langservice as lang
 from ..parser import BaseBlock, BaseParser, BaseTransform, BlockAttrs, BlockData, BlockProgram, BlockState, BlockUnitData, BlockUnitState, FiberParser, Transforms
+from ..process import ProgramExecEvent
+from ...reader import LocationArea
 from ...util import schema as sc
 from ...util.decorators import debug
 
@@ -73,16 +73,23 @@ class SequenceTransform(BaseTransform):
     return analysis, SequenceBlock(children) if children else Ellipsis
 
 
-@debug
+class SequenceProgramMode(IntEnum):
+  Normal = 0
+  Pausing = 1
+  Interrupting = 2
+  Stopped = 3
+
+@dataclass(kw_only=True)
 class SequenceProgramState:
-  def __init__(self, child: Optional[Any] = None, index: int = 0):
-    self.child = child
-    self.index = index
+  child: Optional[Any] = None
+  index: int = 0
+  mode: SequenceProgramMode
 
   def export(self):
     return {
       "child": self.child and self.child.export(),
-      "index": self.index
+      "index": self.index,
+      "node": self.mode
     }
 
 @debug
@@ -95,34 +102,48 @@ class SequenceProgram(BlockProgram):
     self._child_program: BlockProgram
     self._child_index = 0
 
+    self._interrupting = False
+
+  def import_message(self, message: Any):
+    match message["type"]:
+      case "setInterrupt":
+        self.set_interrupt(message["value"])
+
   def pause(self):
+    self._interrupting = False
     self._child_program.pause()
 
+  def set_interrupt(self, value: bool, /):
+    self._interrupting = value
+
   async def run(self, initial_state: Optional[SequenceProgramState]):
-    start_state = initial_state or SequenceProgramState()
+    start_state = initial_state or SequenceProgramState(mode=SequenceProgramMode.Normal)
 
     for child_index, child_block in enumerate(self._block._children):
       if child_index < start_state.index:
         continue
 
-      # if self._paused:
-      #   self._paused = False
-      #
-      #   yield ProgramExecEvent(
-      #     stopped=True,
-      #     state=SequenceProgramState(child=None, index=child_index)
-      #   )
+      if self._interrupting:
+        self._interrupting = False
 
+        yield ProgramExecEvent(
+          state=SequenceProgramState(
+            index=child_index,
+            mode=SequenceProgramMode.Stopped
+          ),
+          stopped=True
+        )
 
       self._child_program = child_block.Program(child_block, self._master, self)
 
       async for info in self._child_program.run(start_state.child if child_index == start_state.index else None):
         yield ProgramExecEvent(
-          pausable=info.pausable,
           state=SequenceProgramState(
+            child=info.state,
             index=child_index,
-            child=info.state
+            mode=SequenceProgramMode.Stopped
           ),
+          stopped=info.stopped,
           time=info.time
         )
 

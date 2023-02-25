92a26630581ad1ddc8fbd42bf888cc0b5c195c82
Simon Liétar <simon@lietar.net>
2023-02-25 21:21:02 +0100

    Improve socket client

--- app/library/src/socket-client.ts
+++ app/library/src/socket-client.ts
@@ -1,11 +1,25 @@
-import * as net from 'net';
+import assert from 'assert';
+import crypto from 'crypto';
+import net from 'net';
+import tls from 'tls';
 
+import * as Protocol from './protocol';
 import { Deferred, defer } from './util';
 
 
 export class SocketClientClosed extends Error {
 
 }
+
+function createErrorWithCode(message: string, code: string) {
+  let err = new Error(message);
+
+  // @ts-expect-error
+  err.code = code;
+
+  return err;
+}
+
 
 export type SocketClientBackendOptions = {
   host: string;
@@ -25,11 +39,38 @@ export class SocketClientBackend {
   isClosed: boolean = false;
   ready: Promise<void>;
 
-  constructor(options: SocketClientBackendOptions) {
+  constructor(options: {
+    address: SocketClientBackendOptions;
+    tls: {
+      clientAuth: null;
+      serverAuth: {
+        type: 'any' | 'system';
+      } | {
+        type: 'fingerprint';
+        fingerprint: string;
+      };
+    } | null;
+  }) {
     this.ready = new Promise((resolve, reject) => {
-      this._socket = net.createConnection(options, () => {
-        resolve();
-      });
+      if (options.tls) {
+        let tlsOptions = options.tls;
+
+        this._socket = tls.connect({
+          ...options.address,
+          rejectUnauthorized: (tlsOptions.serverAuth.type === 'system')
+        }, () => {
+          if ((tlsOptions.serverAuth.type === 'fingerprint') && (this.getServerCertificateFingerprint() !== tlsOptions.serverAuth.fingerprint)) {
+            reject(createErrorWithCode('Invalid fingerprint', 'APP_FINGERPRINT_MISMATCH'));
+            this._socket.destroy();
+          } else {
+            resolve();
+          }
+        });
+      } else {
+        this._socket = net.createConnection(options.address, () => {
+          resolve();
+        });
+      }
 
       this._socket.on('error', (err) => {
         // TODO: Handle errors after this.ready
@@ -57,6 +98,15 @@ export class SocketClientBackend {
         }
       });
     });
+  }
+
+  getServerCertificateFingerprint() {
+    assert(this._socket instanceof tls.TLSSocket);
+
+    let certificate = this._socket.getPeerCertificate();
+    let fingerprint = crypto.createHash('sha256').update(certificate.raw).digest('hex');
+
+    return fingerprint;
   }
 
   async close() {
@@ -101,45 +151,85 @@ export class SocketClientBackend {
 }
 
 
-export class SocketClient extends SocketClientBackend {
-  private _nextRequestId = 0;
-  private _requests = new Map<number, Deferred<unknown>>();
-
-  async start() {
+export class MessageSocketClientBackend extends SocketClientBackend {
+  // @ts-expect-error
+  override async * [Symbol.asyncIterator]() {
     let contents = '';
 
     try {
-      for await (let chunk of this) {
+      for await (let chunk of super[Symbol.asyncIterator]()) {
         contents += chunk.toString();
 
         let msgs = contents.split('\n');
         contents = msgs.at(-1)!;
 
         for (let msg of msgs.slice(0, -1)) {
-          let message = JSON.parse(msg);
+          let message;
 
-          switch (message.type) {
-            case 'response': {
-              let request = this._requests.get(message.id);
+          try {
+            message = JSON.parse(msg) as Protocol.Server.Message;
+          } catch (err) {
+            if (err instanceof SyntaxError) {
+              throw createErrorWithCode('Malformed message', 'APP_MALFORMED');
+            }
 
-              if (request) {
-                request.resolve(message.data);
-                this._requests.delete(message.id);
-              }
+            throw err;
+          }
+
+          yield message;
+        }
+      }
+    } catch (err) {
+      if (!(err instanceof SocketClientClosed)) {
+        throw err;
+      }
+    }
+  }
+}
+
+export class SocketClient extends MessageSocketClientBackend {
+  private _nextRequestId = 0;
+  private _requests = new Map<number, Deferred<unknown>>();
+
+  async initialize() {
+    let createProtocolError = () => createErrorWithCode('Invalid message', 'APP_PROTOCOL');
+
+    let iterator = this[Symbol.asyncIterator]();
+    let item = await iterator.next();
+
+    if (item.done || item.value.type !== 'initialize') {
+      throw createProtocolError();
+    }
+
+    return {
+      identifier: item.value.identifier,
+      version: item.value.version
+    };
+  }
+
+  async start() {
+    try {
+      for await (let message of this) {
+        switch (message.type) {
+          case 'response': {
+            let request = this._requests.get(message.id);
+
+            if (request) {
+              request.resolve(message.data);
+              this._requests.delete(message.id);
             }
           }
         }
       }
-    } catch (err) {
-      if (err instanceof SocketClientClosed) {
-        for (let request of this._requests.values()) {
-          request.reject(err);
-        }
-
-        this._requests.clear();
-      } else {
-        throw err;
+    } catch (err: any) {
+      await this.close();
+      throw err;
+    } finally {
+      for (let request of this._requests.values()) {
+        request.reject(new SocketClientClosed());
       }
+
+      this._requests.clear();
     }
   }
 
@@ -164,12 +254,27 @@ export class SocketClient extends SocketClientBackend {
 
 
   static async test(options: SocketClientBackendOptions) {
-    let client = new SocketClientBackend(options);
+    let client = new SocketClient({
+      address: options,
+      tls: {
+        clientAuth: null,
+        serverAuth: {
+          type: 'any'
+        }
+      }
+    });
 
     try {
       await client.ready;
     } catch (err: any) {
+      console.log('=>', err.code);
+
       switch (err.code) {
+        case 'DEPTH_ZERO_SELF_SIGNED_CERT':
+          return {
+            ok: false,
+            reason: 'unauthorized'
+          } as const;
         case 'ECONNREFUSED':
           return {
             ok: false,
@@ -178,16 +283,33 @@ export class SocketClient extends SocketClientBackend {
         case 'ERR_SOCKET_BAD_PORT':
           return {
             ok: false,
-            reason: 'invalid'
+            reason: 'invalid_parameters'
           } as const;
+/*         case 'APP_FINGERPRINT_MISMATCH':
+          return {
+            ok: false,
+            reason: 'compromised'
+          } */
         default:
           throw err;
       }
     }
 
-    console.log(await client.recv());
+    let initializationResult;
 
-    await client.close();
+    try {
+      initializationResult = await client.initialize();
+    } catch (err: any) {
+      switch (err.code) {
+        case 'APP_PROTOCOL':
+          return {
+            ok: false,
+            reason: 'protocol'
+          }
+      }
+    } finally {
+      await client.close();
+    }
 
     // try {
     //   await client.recv();
@@ -196,7 +318,9 @@ export class SocketClient extends SocketClientBackend {
     // }
 
     return {
-      ok: true
+      ok: true,
+      ...initializationResult,
+      serverCertificateFingerprint: client.getServerCertificateFingerprint()
     } as const;
   }
 }
